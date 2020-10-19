44b1604b0f6bab1031e9e5656619ad843429d8c1
vladimir-gnedko2 <vavanchikmks@gmail.com>
2020-10-19 20:50:14 +0300

    edit solution

--- task19.js
+++ task19.js
@@ -1,3 +1,6 @@
-let countCats = [[0,1,"^^"],[9,"^^",2],["^^",8,7]];
-cats = (params) => params.join().split(',').filter(item => item === "^^"? item : 0).length
-console.log(cats(countCats))
+let cats = [[0,1,"^^"],[9,"^^",2],["^^",8,7]];
+countCats = (params) => params.join()
+                        .split(',')
+                        .filter(item => item === "^^"? item : 0)
+                        .length;
+console.log(countCats(cats));
